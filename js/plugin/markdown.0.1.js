1bc2d42917d37397a651002706cf7c2364d07505
hikaMaeng <hika00@gmail.com>
2014-07-02 18:28:11 +0900

    마크다운코드정리

--- js/plugin/markdown.0.1.js
+++ js/plugin/markdown.0.1.js
@@ -2,14 +2,11 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 var rUrl = /^https?:\/\/[-\w.]+(:[0-9]+)?(\/([\w\/_.#!]*)?)?$/, linkUrls = {}, rQuoteHead = /\n>\s/g, synts = [
 {r:/^\[(.*)\]:\s?(.*)\n/gm, t:function( org, k, v ){// linkUrls
-		return linkUrls['"'+k.toLowerCase()+'"'] = v, '';
-	}
-},
+	return linkUrls['"'+k.toLowerCase()+'"'] = v, '';
+}},
 {r:/^>\s(.*)((\n>\s.*)*)/gm, t:function( org, v1, v2 ){// Blockquotes
-		//console.log(v1+v2.replace(/\n>\s/g, ''));
-		return '<blockquote>'+ (v1+v2.replace( rQuoteHead, ' ' )) +'</blockquote>';
-	}
-},
+	return '<blockquote>'+ (v1+v2.replace( rQuoteHead, ' ' )) +'</blockquote>';
+}},
 {r:/^(.*)$\n={6,}/gm, t:'<h1>$1</h1>'},// alt-h1
 {r:/^(.*)$\n-{6,}/gm, t:'<h2>$1</h2>'},// alt-h2
 {r:/^#{6}\s(.*)$/gm, t:'<h6>$1</h6>'},// h6
@@ -26,61 +23,41 @@ var rUrl = /^https?:\/\/[-\w.]+(:[0-9]+)?(\/([\w\/_.#!]*)?)?$/, linkUrls = {}, r
 {r:/\*(.*)\*/gm, t:'<em>$1</em>'},// em
 {r:/\s{2,}$/gm, t:'<br />'},// br
 {r:/\!(\[(.*)\])?\[(.*)\]/gm, t:function( org, s1, alt, k ){// img ![][]
-		return '!'+(alt?'['+alt+']':'')+'('+linkUrls['"'+k.toLowerCase()+'"']+')';
-	}
-},
+	return '!'+(alt?'['+alt+']':'')+'('+linkUrls['"'+k.toLowerCase()+'"']+')';
+}},
 {r:/\!(\[(.*)\])?\((\S*)\s?(.*)?\)/gm, t:function( org, s1, alt, src, title ){// img ![]()
-		return '<img src="'+src+(alt?'" alt="'+alt:'')+(title?'" title='+title:'')+'></img>';
-	}
-},
+	return '<img src="'+src+(alt?'" alt="'+alt:'')+(title?'" title='+title:'')+'></img>';
+}},
 {r:/\[(.*)\]\[(.*)\]/gm, t:function( org, v, k ){// link [][]
-		return '<a href="'+linkUrls['"'+k.toLowerCase()+'"']+'">'+v+'</a>';
-	}
-},
+	return '<a href="'+linkUrls['"'+k.toLowerCase()+'"']+'">'+v+'</a>';
+}},
 {r:/\[(.*)\]\((.*)\)/gm, t:'<a href="$2">$1</a>'},// link []()
 {r:/\[(.*)\]/gm, t:function( org, k ){// link []
-		return '<a href="'+linkUrls['"'+k.toLowerCase()+'"']+'">'+k+'</a>';
-	}
-},
+	return '<a href="'+linkUrls['"'+k.toLowerCase()+'"']+'">'+k+'</a>';
+}},
 {r:/^(\s{0,})[\*\+-]{1}\s(.*)((\n{1}\s{0,}[\*\+-]{1}\s.+)*)/gm, t:(function(){
-		var rInfo = /^(\s{0,})[\*\+-]{1}\s(.*)/;
-		return function( org, v0, v1, v2 ){
-			var ret, rsps, psps, nsps, sps, spsln, cntnt,
-				t0, t1, i, j;
-			ret = '<li>'+ v1 +'</li>';
-			rsps = v0;
-			psps = -1;
-			console.log(111111);
-			console.log(v2);
-			t0 = v2.split('\n');
-			for( i = 1, j = t0.length; i < j; i++ ){
-				t1 = rInfo.exec(t0[i]);
-				sps = t1[1];
-				spsln = sps.length
-				cntnt = t1[2];
-				if(i < j-1) nsps = rInfo.exec(t0[i+1])[1].length;
-				else nsps = -1;
-				if( sps != rsps ){
-					if( spsln > psps ) ret += '<ul>';
-				}
-				ret += '<li>'+ cntnt +'</li>';
-				if( sps != rsps ){
-					if( spsln > nsps ) ret += '</ul>';
-					psps = spsln;
-				}
+	var rInfo = /^(\s{0,})[\*\+-]{1}\s(.*)/;
+	return function( org, v0, v1, v2 ){
+		var ret, rsps, psps, nsps, sps, spsln, cntnt, t0, t1, i, j;
+		for( ret = '<li>'+ v1 +'</li>', rsps = v0, psps = -1, t0 = v2.split('\n'), i = 1, j = t0.length; i < j; i++ ){
+			t1 = rInfo.exec(t0[i]), sps = t1[1], spsln = sps.length, cntnt = t1[2],
+			nsps = i < j - 1 ? rInfo.exec(t0[i+1])[1].length : -1;
+			if( sps != rsps && spsln > psps ) ret += '<ul>';
+			ret += '<li>'+ cntnt +'</li>';
+			if( sps != rsps ){
+				if( spsln > nsps ) ret += '</ul>';
+				psps = spsln;
 			}
-			ret = '<ul>'+ ret +'</ul>\n';
-			return ret;
-		};
-	})()
-},
-{r:/^(\s{0,})[\*\+-]{1}\s(.*)/gm, t:function( org, v0, v1, v2 ){
-		var ret;
-		ret = '<ul><li>'+ v1 +'</li></ul>';
-		
+		}
+		ret = '<ul>'+ ret +'</ul>\n';
 		return ret;
-	}
-},
+	};
+})()},
+{r:/^(\s{0,})[\*\+-]{1}\s(.*)/gm, t:function( org, v0, v1, v2 ){
+	var ret;
+	ret = '<ul><li>'+ v1 +'</li></ul>';
+	return ret;
+}},
 {r:/^(.*)\n{2}/gm, t:'<p>$1</p>\n'}// p
 ];
 exports.markdown = function(data){
