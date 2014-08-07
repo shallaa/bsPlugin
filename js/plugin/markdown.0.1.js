857ba9c5595cc5399ac2a6c2b5be8c136b6374c7
SeungHyun PAEK <pekuid@gmail.com>
2014-08-07 10:52:47 +0900

    [][] 버그수정

--- js/plugin/markdown.0.1.js
+++ js/plugin/markdown.0.1.js
@@ -3,10 +3,10 @@
 var resLoaded = 0, linkUrls = {}, rQuoteHead = /\n>\s/g, rln = /\n|\r\n|\r/gm,
 synts = [
 {r:/^\[(.*)\]:\s?(.*)\n/gm, t:function( org, k, v ){// linkUrls
-	return linkUrls['"'+k.toLowerCase()+'"'] = v, '';
+	return linkUrls['"' + k.toLowerCase() + '"'] = v, '';
 }},
 {r:/^>\s(.*)((\n>\s.*)*)/gm, t:function( org, v1, v2 ){// Blockquotes
-	return '<blockquote class="bsmarkdown">'+ (v1+v2.replace( rQuoteHead, ' ' )) +'</blockquote>';
+	return '<blockquote class="bsmarkdown">' + ( v1 + v2.replace( rQuoteHead, ' ' ) ) + '</blockquote>';
 }},
 {r:/^(.*)$\n={6,}/gm, t:'<h1 class="bsmarkdown">$1</h1>'},// alt-h1
 {r:/^(.*)$\n-{6,}/gm, t:'<h2 class="bsmarkdown">$1</h2>'},// alt-h2
@@ -29,10 +29,7 @@ synts = [
 		};
 	return function(org, s1, s2){
 		return '<pre class="bsmarkdown"><code class="' + (syntax[s1] || 'js') + '">' + (!s2 ? '' : s2.replace( re1, '&amp;' )
-			.replace( re2, '&gt;' )
-			.replace( re3, '&lt;' )
-			.replace( re4, '&quot;' )
-			.replace( re5, '&apos;' )) + '</code></pre>';
+			.replace( re2, '&gt;' ).replace( re3, '&lt;' ).replace( re4, '&quot;' ).replace( re5, '&apos;' )) + '</code></pre>';
 	}
 })()},// pre, code
 {r:/```(.*)```/gm, t:(function(){
@@ -41,10 +38,7 @@ synts = [
 		re5 = /'/g;
 	return function(org, s1){
 		return '<code class="js">' + (!s1 ? '' : s1.replace( re1, '&amp;' )
-			.replace( re2, '&gt;' )
-			.replace( re3, '&lt;' )
-			.replace( re4, '&quot;' )
-			.replace( re5, '&apos;' )) + '</code>';
+			.replace( re2, '&gt;' ).replace( re3, '&lt;' ).replace( re4, '&quot;' ).replace( re5, '&apos;' )) + '</code>';
 	}
 })()},// pre, code
 {r:/__(.*)__/gm, t:'<strong class="bsmarkdown">$1</strong>'},// strong
@@ -53,39 +47,41 @@ synts = [
 {r:/\*(.*)\*/gm, t:'<em class="bsmarkdown">$1</em>'},// em
 {r:/\s{2,}$/gm, t:'<br />'},// br
 {r:/\!(\[(.*)\])?\[(.*)\]/gm, t:function( org, s1, alt, k ){// img ![][]
-	return '!'+(alt?'['+alt+']':'')+'('+linkUrls['"'+k.toLowerCase()+'"']+')';
+	return '!' + (alt ? '[' + alt + ']' : '') + '(' + linkUrls['"' + k.toLowerCase() + '"'] + ')';
 }},
 {r:/\!(\[(.*)\])?\((\S*)\s?(.*)?\)/gm, t:function( org, s1, alt, src, title ){// img ![]()
-	return '<img class="bsmarkdown" src="'+src+(alt?'" alt="'+alt:'')+(title?'" title='+title:'')+'></img>';
+	return '<img class="bsmarkdown" src="' + src + (alt ? '" alt="' + alt : '') + (title ? '" title=' + title : '') + '></img>';
 }},
 {r:/\[(.*)\]\[(.*)\]/gm, t:function( org, v, k ){// link [][]
-	return '<a class="bsmarkdown" href="'+linkUrls['"'+k.toLowerCase()+'"']+'">'+v+'</a>';
+	var t0 = linkUrls['"' + k.toLowerCase() + '"'];
+	return t0 ? '<a class="bsmarkdown" href="' + t0 + '">' + v + '</a>' : org;
 }},
 {r:/\[(.*)\]\((.*)\)/gm, t:'<a href="$2">$1</a>'},// link []()
 {r:/\[(.*)\]/gm, t:function( org, k ){// link []
-	return '<a class="bsmarkdown" href="'+linkUrls['"'+k.toLowerCase()+'"']+'">'+k+'</a>';
+	var t0 = linkUrls['"' + k.toLowerCase() + '"'];
+	return t0 ? '<a class="bsmarkdown" href="' + t0 + '">' + k + '</a>' : org;
 }},
 {r:/^(\s{0,})[\*\+-]{1}\s(.*)((\n{1}\s{0,}[\*\+-]{1}\s.+)*)/gm, t:(function(){
 	var rInfo = /^(\s{0,})[\*\+-]{1}\s(.*)/;
 	return function( org, v0, v1, v2 ){
 		var ret, rsps, psps, nsps, sps, spsln, cntnt, t0, t1, i, j;
-		for( ret = '<li class="bsmarkdown">'+ v1 +'</li>', rsps = v0, psps = -1, t0 = v2.split('\n'), i = 1, j = t0.length; i < j; i++ ){
+		for( ret = '<li class="bsmarkdown">' + v1 + '</li>', rsps = v0, psps = -1, t0 = v2.split('\n'), i = 1, j = t0.length; i < j; i++ ){
 			t1 = rInfo.exec(t0[i]), sps = t1[1], spsln = sps.length, cntnt = t1[2],
 			nsps = i < j - 1 ? rInfo.exec(t0[i+1])[1].length : -1;
 			if( sps != rsps && spsln > psps ) ret += '<ul class="bsmarkdown">';
-			ret += '<li class="bsmarkdown">'+ cntnt +'</li>';
+			ret += '<li class="bsmarkdown">' + cntnt + '</li>';
 			if( sps != rsps ){
 				if( spsln > nsps ) ret += '</ul>';
 				psps = spsln;
 			}
 		}
-		ret = '<ul class="bsmarkdown">'+ ret +'</ul>\n';
+		ret = '<ul class="bsmarkdown">' + ret + '</ul>\n';
 		return ret;
 	};
 })()},
 {r:/^(\s{0,})[\*\+-]{1}\s(.*)/gm, t:function( org, v0, v1, v2 ){
 	var ret;
-	ret = '<ul class="bsmarkdown"><li class="bsmarkdown">'+ v1 +'</li></ul>';
+	ret = '<ul class="bsmarkdown"><li class="bsmarkdown">' + v1 + '</li></ul>';
 	return ret;
 }},
 {r:/^(.*)\n{2}/gm, t:'<p class="bsmarkdown">$1</p>\n'}// p
